30283edcae4c612ef8733dac911ffee16c9fa7ea
LAYLA GIOVANNA FURTADO DA SILVA <22282096@dsn127m1242376.cfp127aluno.local>
2023-08-17 11:07:51 -0300

    Iniciando a landing page com React

---
+++ src/components/Elipse/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//imports
+import styles from './styles.css'
+
+export default function Elipse(){
+    return(
+        <>
+            <h1>Elipse</h1>
+        </>
+    )
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//imports
+import styles from './styles.css';
+
+import Elipse from "../../components/Elipse";
+
+export default function Home(){
+    return(
+        <>
+            <h1>HOME</h1>
+            <Elipse />
+        </>
+    )
+}
